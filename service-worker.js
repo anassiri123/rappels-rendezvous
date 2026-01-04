d4f362b1a8cdd5d12f179f240c281cd89e1db22b
anassiri123 <anassiriabderahim1982@gmail.com>
2026-01-04 14:35:07 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,36 +2,25 @@ self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  self.clients.claim();
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
-/* 🔔 NOTIFICATION */
-self.addEventListener("push", event => {
-  const data = event.data.json();
-
-  self.registration.showNotification("⏰ Rappel", {
-    body: data.text,
-    icon: "icon-192.png",
-    badge: "icon-192.png",
-    vibrate: [200, 100, 200],
-    data: {
-      id: data.id
-    }
-  });
-});
-
-/* 👉 CLIC SUR LA NOTIFICATION */
-self.addEventListener("notificationclick", event => {
+// Réception notification
+self.addEventListener("notificationclick", function (event) {
   event.notification.close();
 
   event.waitUntil(
-    clients.matchAll({ type: "window", includeUncontrolled: true }).then(clientsArr => {
-      if (clientsArr.length > 0) {
-        clientsArr[0].focus();
-      } else {
-        clients.openWindow("./index.html");
-      }
-    })
+    self.clients.matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url.includes("rappels-rendezvous") && "focus" in client) {
+            return client.focus();
+          }
+        }
+        if (self.clients.openWindow) {
+          return self.clients.openWindow("/rappels-rendezvous/");
+        }
+      })
   );
 });
